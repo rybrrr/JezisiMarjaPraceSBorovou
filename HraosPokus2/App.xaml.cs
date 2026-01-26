e6e3c1148363b5c68aee5cdb22646c8ee78f4bd0
rybrrr <kopejskat@gchd.cz>
2026-01-26 08:26:33 +0100

    Initialize WPF project with main window
    Added initial files for a new WPF application targeting .NET 8.0. Includes App.xaml, MainWindow.xaml, project file, and assembly info.

---
+++ HraosPokus2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HraosPokus2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
